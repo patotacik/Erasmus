b7fd89677ed408b0ea40ed5e14583260d6d61a2d
Tomáš <bielikto@gmail.com>
2019-11-11 22:28:42 +0100

    tomáš

---
+++ resources/views/details.blade.php
@@ -0,0 +1,27 @@
+@extends('layout.mainlayout')
+<div class="hero-wrap ftco-degree-bg" style="background-image: url('images/uvod.png');"
+     data-stellar-background-ratio="0.5" xmlns:color="http://www.w3.org/1999/xhtml">            <div class="overlay"></div>
+    <div class="container">
+        <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center">
+            <div class="col-md-9 ftco-animate pb-5 text-center">
+            </div>
+        </div>
+    </div>
+</div>
+    </section>
+
+    @section('content')
+
+        <section class="ftco-section">
+            <div style="color:black;padding-left:15%;padding-right: 15%">
+                <h2><b>London</b></h2>
+                <p style="align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse velit nibh, vulputate at neque nec, laoreet tincidunt magna. Maecenas auctor ipsum a mi aliquam, sed scelerisque mi vestibulum. Quisque id dolor a ante varius iaculis nec sit amet velit. Vivamus sed ultricies tellus. Maecenas suscipit, quam non varius porttitor, massa massa volutpat enim, sed ultricies neque tellus sit amet neque. Etiam aliquet erat et turpis auctor, vel luctus nunc congue. Donec rhoncus maximus turpis, vitae maximus dui facilisis porttitor. Pellentesque eu egestas justo. Nam auctor, lorem in aliquet dignissim, tellus metus sagittis justo, at sodales eros arcu ac risus.
+
+                    Suspendisse nec congue dolor, vel tincidunt sem. Cras vehicula orci eget justo facilisis scelerisque. Pellentesque eget odio pharetra, elementum lacus eget, commodo velit. Suspendisse facilisis ullamcorper metus ut porttitor. Phasellus condimentum porttitor tristique. Donec purus massa, laoreet et convallis ut, consectetur id dolor. Duis sagittis aliquet tempor.</p>
+
+            </div>
+            <div class="center" style="padding-left:15%; padding-right: 15%">
+                <img src="images/work-1.jpg" alt="nananana">
+            </div>
+        </section>
+@endsection
