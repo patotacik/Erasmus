cdda9803d2943ff0bd03a05b23245877ead1ce13
patotacik <patino122@azet.sk>
2019-11-26 09:47:31 +0100

    obrazok

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.mainlayout')
 @section('content')
-    <div class="hero-wrap ftco-degree-bg" style="background-image: url({{ URL::asset('images/uvod.png')}});"
+    <div class="hero-wrap ftco-degree-bg" style="background-image: url({{ URL::asset('images/detail.png')}});"
          data-stellar-background-ratio="0.5" xmlns:color="http://www.w3.org/1999/xhtml">
     </div>
     </section>
