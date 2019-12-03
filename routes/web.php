e65b5b33437ebe05d8a9819e438edd2c7080bc0b
Jesusko6 <48484119+Jesusko6@users.noreply.github.com>
2019-12-03 12:19:36 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,30 +12,35 @@
 Route::get('/', [
     'as' => 'show', 'uses' =>'PagesController@getIndex',
 ]);
-
 Route::get('/users/create', 'AdminController@create')->name('users.create');
 
 Route::get('/showa/{id}', [
     'as' => 'showa', 'uses' => 'AdminController@show']);
 
 Route::delete('/users/{id}', 'AdminController@destroy')->name('users.destroy');
+Route::delete('/events/{id}', 'AdminController@EventDestroy')->name('events.destroy');
+Route::delete('/univerzity/{id}', 'AdminController@UniverzityDestroy')->name('univerzity.destroy');
 
 Route::post('/users', 'AdminController@store')->name('users.store');
+Route::post('/events', 'AdminController@EventStore')->name('events.store');
+Route::post('/univerzity', 'AdminController@UniverzityStore')->name('univerzity.store');
 
 Route::get('/users/{id}/edit', 'AdminController@edit')->name('users.edit');
+Route::get('/events/{id}/edit', 'AdminController@EventEdit')->name('events.edit');
+Route::get('/univerzity/{id}/edit', 'AdminController@UniverzityEdit')->name('univerzity.edit');
 
 Route::put('users/{id}', 'AdminController@update')->name('users.update');
-
+Route::put('events/{id}', 'AdminController@EventUpdate')->name('events.update');
+Route::put('univerzity/{id}', 'AdminController@UniverzityUpdate')->name('univerzity.update');
 
 Route::get('/erasmus', 'PodujatiaController@getPodujatia');
+
 Route::get('/details/{id}', 'detailsController@getErasmusId')->name('erasmus.details');
-
-
-
 
 Route::get('/admin', 'AdminController@admin')
     ->middleware('is_admin')
     ->name('admin');
+
 Route::get('/edit', 'AdminController@table')
     ->middleware('is_admin')
     ->name('edit');
@@ -45,28 +49,27 @@ Route::get('/referent', 'AdminController@referent')
     ->middleware('is_admin')
     ->name('referent');
 
-
 Route::get('/ucasnik', 'AdminController@ucasnik')
     ->middleware('is_ucasnik')
     ->name('ucasnik');
+
 Route::get('/seminar', 'AdminController@ucasnik_inf_sem')
     ->middleware('is_ucasnik')
     ->name('seminar');
+
 Route::get('/ziadosti', 'AdminController@ziadosti')
     ->middleware('is_ucasnik')
     ->name('ziadosti');
+
 Route::get('/formular/{id}', 'AdminController@formular')
     ->middleware('is_ucasnik')
     ->name('potvrdene.formular');
 
 Route::post('/pridat', 'AdminController@pridat')->name('formular.pridat');
 
-
-
 Route::get('/potvrdene/{users_id}', 'AdminController@getPatvdeneId')
     ->middleware('is_ucasnik')
     ->name('potvrdenie.details');
-
 
 Route::get('/show/{id}', [
     'as' => 'show', 'uses' => 'AdminController@ShowAction'
@@ -84,14 +87,22 @@ Route::get('/AllUsers', [
     'as' => 'AllUsers', 'uses' => 'AdminController@table'
 ]);
 
+Route::get('/AllEvents', [
+    'as' => 'AllEvents', 'uses' => 'AdminController@EventsTable'
+]);
+
+Route::get('/AllUniverzity', [
+    'as' => 'AllUniverzity', 'uses' => 'AdminController@UniverzityTable'
+]);
+
 Route::post('/update/{id}', [
     'as' => 'update', 'uses' => 'AdminController@updateAction'
 ]);
 
-
 Route::get('kontakt', [
-   'as' => 'show', 'uses' =>'PagesController@getKontakt'
+    'as' => 'show', 'uses' =>'PagesController@getKontakt'
 ]);
+
 Route::get('index', [
     'as' => 'show', 'uses' =>'PagesController@getIndex',
 ]);
@@ -99,15 +110,19 @@ Route::get('index', [
 Route::get('login', [
     'as' => 'show', 'uses' =>'PagesController@getLogin'
 ]);
+
 Route::get('staz', [
     'as' => 'show', 'uses' =>'PagesController@getStaz'
 ]);
+
 Route::get('faktultný_koordinatori', [
     'as' => 'show', 'uses' =>'PagesController@getfaktkoord'
 ]);
+
 Route::get('odd_medzin_vzt', [
     'as' => 'show', 'uses' =>'PagesController@getodd_medzin_vzt'
 ]);
+
 Auth::routes();
 Route::get('/home', 'PagesController@getIndex')->name('home');
 Route::get('/uploadfile', 'UploadfileController@index');
@@ -116,6 +131,5 @@ Route::get('/main', 'MainController@index');
 Route::post('/main/checklogin', 'MainController@checklogin');
 Route::get('main/successlogin', 'MainController@successlogin');
 Route::get('main/logout', 'MainController@logout');
-
 Route::get('contactus', 'ContactUsController@getContactus');
 Route::post('contactus', 'ContactUsController@postContactus')->name('contactus');
