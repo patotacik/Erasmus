fa8880c0cdc9315f2f94116c55fe789937230579
patotacik <patino122@azet.sk>
2019-12-04 20:41:37 +0100

    fakult_koord

--- resources/views/fakult_koord.blade.php
+++ resources/views/fakult_koord.blade.php
@@ -9,8 +9,50 @@
         <div class="container">
             <div class="row align-items-start">
                 <div class="col-md-12 heading-section text-center ftco-animate mb-5">
-                    <span class="subheading">Ponuka erazmus+</span>
-                    <h2 class="mb-2">Najžiadanejšie ponuky</h2>
+                    <span class="subheading">Kontakty</span>
+                    <h2 class="mb-2">Fakultný koordinátori</h2>
                 </div>
+                <div class="container">
+                    <table class="table table-dark table-striped">
+                        <thead>
+                        <tr>
+                            <th>Meno</th>
+                            <th>Mobilný telefon</th>
+                            <th>Email</th>
+                            <th>Fakulta:</th>
 
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>Mgr. Martin Drlík, PhD.</td>
+                            <td>037/6408 686</td>
+                            <td>mdrlik@ukf.sk</td>
+                            <td>Fakulta prírodných vied</td>
+
+                        </tr>
+                        <tr>
+                            <td>prof. PaedDr. Bernard Garaj, CSc.</td>
+                            <td>037/6408 451</td>
+                            <td>bgaraj@ukf.sk</td>
+                            <td>Filozofická fakulta</td>
+
+                        </tr>
+                        <tr>
+                            <td>doc. PhDr. Adriana Récka, PhD.</td>
+                            <td>037/6408 224</td>
+                            <td>arecka@ukf.sk</td>
+                            <td>Pedagogická fakulta</td>
+
+                        </tr>
+                        <tr>
+                            <td>PhDr. Miroslava Čerešníková, PhD.</td>
+                            <td>037/6408 779</td>
+                            <td>mceresnikova@ukf.sk</td>
+                            <td>Fakulta sociálnych vied a zdravotníctva</td>
+
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
 @endsection
