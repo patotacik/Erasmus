4402ca404636ae67ff0762e0aa79c98020c83ab5
Vagunda Matus <37534175+GatorKing@users.noreply.github.com>
2019-12-14 17:53:46 +0100

    zmeny v db

--- database/seeds/UsersTablesSeeder.php
+++ database/seeds/UsersTablesSeeder.php
@@ -11,9 +11,9 @@ class UsersTableSeeder extends Seeder
     {
         DB::table('users')->delete();
         $users = array(
-            array('id' => 1,'name' => "Admin Smith",'email' => "admin@gmail.com",'password' =>  Hash::make('password'),'remember_token' =>  str_random(10),'rolies_id' => 1),
-            array('id' => 2,'name' => "Referent Smith",'email' => "referent@gmail.com",'password' =>  Hash::make('password'),'remember_token' =>  str_random(10),'rolies_id' => 2),
-            array('id' => 3,'name' => "Ucastnik Smith",'email' => "ucastnik@gmail.com",'password' =>  Hash::make('password'),'remember_token' =>  str_random(10),'rolies_id' => 3),
+            array('id' => 1,'name' => "Admin Smith",'email' => "admin@gmail.com",'password' =>  Hash::make('password'),'remember_token' =>  str_random(10),'roly_id' => 1),
+            array('id' => 2,'name' => "Referent Smith",'email' => "referent@gmail.com",'password' =>  Hash::make('password'),'remember_token' =>  str_random(10),'roly_id' => 2),
+            array('id' => 3,'name' => "Ucastnik Smith",'email' => "ucastnik@gmail.com",'password' =>  Hash::make('password'),'remember_token' =>  str_random(10),'roly_id' => 3),
 
 
 
