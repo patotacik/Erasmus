d63a4f326b73bdf37677a78629b9caa9fd7b4359
Jesusko6 <JesuskoSupaChloor@gmail.com>
2019-12-16 19:49:29 +0100

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,6 +10,7 @@ use App\Roly;
 use App\Vyzvi;
 use Illuminate\Http\Request;
 use App\User;
+use PDF;
 class AdminController extends Controller
 {
     public function __construct()
@@ -146,7 +147,8 @@ class AdminController extends Controller
     public function Eventstable()
     {
         $podujatia = podujatia::latest()->paginate(5);
-        return view('admin/admin_EventsTable', ['podujatia' => $podujatia])
+        $Epod = Podujatia::all();
+        return view('admin/admin_EventsTable', ['podujatia' => $podujatia],['Epod' => $Epod])
             ->with('i', (request()->input('page', 1) - 1) * 5);
 
     }
@@ -162,7 +164,8 @@ class AdminController extends Controller
     public function Univerzitytable()
     {
         $univerzity = univerzity::latest()->paginate(5);
-        return view('admin/admin_UniverzityTable', ['univerzity' => $univerzity])
+        $Euni = univerzity::all();
+        return view('admin/admin_UniverzityTable', ['univerzity' => $univerzity],['Euni' => $Euni])
             ->with('i', (request()->input('page', 1) - 1) * 5);
 
     }
@@ -170,6 +173,7 @@ class AdminController extends Controller
     public function EventStore(Request $request)
     {
         //
+        $Epod = Podujatia::all();
         $podujatia = new Podujatia();
         $podujatia->Nazov = $request->Nazov;
         $podujatia->datum = $request->datum;
@@ -207,6 +211,7 @@ class AdminController extends Controller
         $univerzity->nazov = $request->nazov;
         $univerzity->zaciatok = $request->zaciatok;
         $univerzity->koniec = $request->koniec;
+        $univerzity->mestos_id = $request->mestos_id;
         $univerzity->krajinies_id = $request->krajinies_id;
         $univerzity->save();
 
@@ -254,22 +259,24 @@ class AdminController extends Controller
     {
         //
         $podujatia = Podujatia::find($id);
-        $sel = Podujatia::all();
+        $Epod = Podujatia::all();
 
-        return view('admin/podujatia/edit', ['podujatie' => $podujatia],['sel' => $sel]);
+        return view('admin/podujatia/edit', ['podujatie' => $podujatia],['Epod' => $Epod]);
     }
 
     public function VyzvaEdit($id)
     {
         //
         $vyzvi = vyzvi::find($id);
+
         return view('admin/vyzvi/edit', ['vyzva' => $vyzvi]);
     }
     public function UniverzityEdit($id)
     {
         //
         $univerzity = univerzity::find($id);
-        return view('admin/univerzity/edit', ['univerzity' => $univerzity]);
+        $Euni = univerzity::all();
+        return view('admin/univerzity/edit', ['univerzity' => $univerzity],['Euni' => $Euni]);
     }
     public function update(Request $request, $id)
     {
@@ -445,5 +452,197 @@ class AdminController extends Controller
         $data = Podujatia::where('users_id', 'like', '%' . $id . '%')
             ->where('confirmed',null)->get();
         return view('u_ziadosti', ['details' => $data]);
+    }
+
+
+
+
+
+
+
+    function index()
+    {
+        $user_data = $this->get_user_data();
+        return view('dynamic_pdf')->with('user_data', $user_data);
+    }
+
+    function get_user_data()
+    {
+        $user_data = User::all();
+        return $user_data;
+    }
+
+    function pdfUser()
+    {
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadHTML($this->convert_user_data_to_html());
+        return $pdf->stream();
+    }
+
+    function convert_user_data_to_html()
+    {
+        $user_data = $this->get_user_data();
+        $output = '
+        <h3 align="center">Uživatelia</h3>
+        <table width="100%" style="border-collapse: collapse; border: 0px;">
+        <tr>
+            <th style="border: 1px solid; padding:12px;" width="8%">id</th>
+            <th style="border: 1px solid; padding:12px;" width="36%">Meno</th>
+            <th style="border: 1px solid; padding:12px;" width="36%">Email</th>
+            <th style="border: 1px solid; padding:12px;" width="20%">Rola</th>
+        </tr>
+     ';
+        foreach($user_data as $user)
+        {
+            $output .= '
+            <tr>
+            <td style="border: 1px solid; padding:12px;">'.$user->id.'</td>
+            <td style="border: 1px solid; padding:12px;">'.$user->name.'</td>
+            <td style="border: 1px solid; padding:12px;">'.$user->email.'</td>
+            <td style="border: 1px solid; padding:12px;">'.$user->rola_nazov->rola.'</td>
+            </tr>
+            ';
+        }
+        $output .= '</table>';
+        return $output;
+    }
+
+    function get_event_data()
+{
+    $event_data = Podujatia::all();
+    return $event_data;
+}
+
+    function pdfEvent()
+    {
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadHTML($this->convert_event_data_to_html());
+        return $pdf->stream();
+    }
+
+    function convert_event_data_to_html()
+    {
+        $event_data = $this->get_event_data();
+        $output = '
+        <h3 align="center">Podujatia</h3>
+        <table width="100%" style="border-collapse: collapse; border: 0px;">
+        <tr>
+            <th style="border: 1px solid; padding:12px;" width="4%">id</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Názov</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Dátum</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Mesto</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Univerzita</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Krajina</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Uživatel</th>
+        </tr>
+        ';
+        foreach($event_data as $event)
+        {
+            $output .= '
+      <tr>
+        <td style="border: 1px solid; padding:12px;">'.$event->id.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$event->Nazov.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$event->datum.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$event->podMes->name.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$event->podUni->nazov.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$event->podKraj->name.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$event->podUser->name.'</td>
+      </tr>
+      ';
+        }
+        $output .= '</table>';
+        return $output;
+    }
+
+    function get_vyzva_data()
+{
+    $vyzva_data = vyzvi::all();
+    return $vyzva_data;
+}
+
+    function pdfVyzva()
+    {
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadHTML($this->convert_vyzva_data_to_html());
+        return $pdf->stream();
+    }
+
+    function convert_vyzva_data_to_html()
+    {
+        $vyzva_data = $this->get_vyzva_data();
+        $output = '
+        <h3 align="center">Výzvy</h3>
+        <table width="100%" style="border-collapse: collapse; border: 0px;">
+        <tr>
+            <th style="border: 1px solid; padding:12px;" width="4%">id</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Typ</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Detail</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Názov</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Mesto</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Cas</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Dátum</th>
+        </tr>
+        ';
+        foreach($vyzva_data as $vyzva)
+        {
+            $output .= '
+      <tr>
+        <td style="border: 1px solid; padding:12px;">'.$vyzva->id.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$vyzva->Typ.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$vyzva->detail.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$vyzva->nazov_seminara.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$vyzva->miesto_konania.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$vyzva->cas_konanie.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$vyzva->datum_konania.'</td>
+      </tr>
+      ';
+        }
+        $output .= '</table>';
+        return $output;
+    }
+
+    function get_uni_data()
+    {
+        $uni_data = univerzity::all();
+        return $uni_data;
+    }
+
+    function pdfUni()
+    {
+        $pdf = \App::make('dompdf.wrapper');
+        $pdf->loadHTML($this->convert_uni_data_to_html());
+        return $pdf->stream();
+    }
+
+    function convert_uni_data_to_html()
+    {
+        $uni_data = $this->get_uni_data();
+        $output = '
+        <h3 align="center">Univerzity</h3>
+        <table width="100%" style="border-collapse: collapse; border: 0px;">
+        <tr>
+            <th style="border: 1px solid; padding:12px;" width="4%">id</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Názov</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Zaciatok Spolupráce</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Koniec Spolupráce</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Mesto</th>
+            <th style="border: 1px solid; padding:12px;" width="16%">Krajina</th>
+        </tr>
+        ';
+        foreach($uni_data as $uni)
+        {
+            $output .= '
+      <tr>
+        <td style="border: 1px solid; padding:12px;">'.$uni->id.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$uni->nazov.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$uni->zaciatok.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$uni->koniec.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$uni->podMes->name.'</td>
+        <td style="border: 1px solid; padding:12px;">'.$uni->podKraj->name.'</td>
+      </tr>
+      ';
+        }
+        $output .= '</table>';
+        return $output;
     }
 }
