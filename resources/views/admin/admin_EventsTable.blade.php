d2d97a64bad90bc6c794f268db76942d67f63d3c
Jesusko6 <JesuskoSupaChloor@gmail.com>
2019-12-16 19:44:47 +0100

    update

--- resources/views/admin/admin_EventsTable.blade.php
+++ resources/views/admin/admin_EventsTable.blade.php
@@ -10,7 +10,8 @@
                 <div class="col-md-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4 class="card-title">Tabulka Podujati</h4>
+                            <h4 class="card-title">Podujatia</h4>
+                            <a href="{{ url('dynamic_pdf/pdfEvent') }}" class="btn btn-danger">Generovať report</a>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
@@ -18,13 +19,13 @@
                                     <thead>
                                     <tr class="text-center">
                                         <th scope="col" class="">#</th>
-                                        <th scope="col" class="">Nazov</th>
-                                        <th scope="col" class="">datum</th>
+                                        <th scope="col" class="">Názov</th>
+                                        <th scope="col" class="">Dátum</th>
                                         <th scope="col" class="">Mesto</th>
-                                        <th scope="col" class="">univerzities_id</th>
-                                        <th scope="col" class="">krajinies_id</th>
-                                        <th scope="col" class="">users_id</th>
-                                        <th scope="col" class="">confirmed</th>
+                                        <th scope="col" class="">Univerzita</th>
+                                        <th scope="col" class="">Krajina</th>
+                                        <th scope="col" class="">Uživatel</th>
+                                        <th scope="col" class="">Potvrdene</th>
                                     </tr>
                                     </thead>
                                     <tbody>
@@ -36,7 +37,7 @@
                                             <td>{{$podujatie->podMes->name}}</td>
                                             <td>{{$podujatie->podUni->nazov}}</td>
                                             <td>{{$podujatie->podKraj->name}}</td>
-                                            <td>{{$podujatie->users_id}}</td>
+                                            <td>{{$podujatie->podUser->name}}</td>
                                             <td>{{$podujatie->confirmed}}</td>
                                             <td class="d-flex align-items-center justify-content-around">
                                                 <form action="{{route('events.edit',$podujatie->id)}}" method="get">
@@ -69,45 +70,83 @@
                             <form action="{{route('events.store')}}" method="post">
                                 <input type="hidden" name="_method" value="post" /> {{csrf_field()}}
                                 <div class="form-group ">
-                                    <label class="text-uppercase font-weight-bold" for="Nazov">Nazov</label>
-                                    <input type="text" class="form-control rounded-0" id="Nazov" placeholder="Nazov" name="Nazov">
+                                    <label class="text-uppercase font-weight-bold" for="Nazov">Názov</label>
+                                    <input type="text" class="form-control rounded-0" id="Nazov" placeholder="Názov" name="Nazov">
                                 </div>
                                 <div class="form-group ">
-                                    <label class="text-uppercase font-weight-bold" for="datum">datum</label>
-                                    <input type="date" class="form-control rounded-0" id="datum" placeholder="Datum" name="datum">
+                                    <label class="text-uppercase font-weight-bold" for="datum">Dátum</label>
+                                    <input type="date" class="form-control rounded-0" id="datum" placeholder="Dátum" name="datum">
+                                </div>
+
+                                <div class="form-field">
+                                    <label class="text-uppercase font-weight-bold" for="mestos_id">Mesto</label>
+                                    <div class="select-wrap">
+                                        <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                                        <select name="mestos_id" class="form-control">
+
+                                            <option value=" "> </option>
+                                            @foreach($Epod as $row)
+                                                <option value="{{$row->mestos_id}}">{{$row->podMes->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group ">
+                                    <label class="text-uppercase font-weight-bold" for="univerzities_id">Univerzita</label>
+                                    <div class="select-wrap">
+                                        <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                                        <select name="univerzities_id" class="form-control">
+                                            <option value=" "> </option>
+                                            @foreach($Epod as $row)
+                                                <option value="{{$row->univerzities_id}}">{{$row->podUni->nazov}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group ">
+                                    <label class="text-uppercase font-weight-bold" for="krajinies_id">Krajina</label>
+                                    <div class="select-wrap">
+                                        <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                                        <select name="krajinies_id" class="form-control">
+                                            <option value=" "> </option>
+                                            @foreach($Epod as $row)
+                                                <option value="{{$row->krajinies_id}}">{{$row->podKraj->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group ">
+                                    <label class="text-uppercase font-weight-bold" for="users_id">Uživatel</label>
+                                    <div class="select-wrap">
+                                        <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                                        <select name="users_id" class="form-control">
+                                            <option value=" "> </option>
+                                            @foreach($Epod as $row)
+                                                <option value="{{$row->users_id}}">{{$row->podUser->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="form-group ">
+                                    <label class="text-uppercase font-weight-bold" for="popis">Popis</label>
+                                    <input type="text" class="form-control rounded-0" id="popis" placeholder="Popis" name="popis">
                                 </div>
                                 <div class="form-group ">
-                                    <label class="text-uppercase font-weight-bold" for="mestos_id">mestos_id</label>
-                                    <input type="number" class="form-control rounded-0" id="mestos_id" placeholder="mestos_id" name="mestos_id">
+                                    <label class="text-uppercase font-weight-bold" for="Obrazok">Obrázok</label>
+                                    <input type="text" class="form-control rounded-0" id="Obrazok" placeholder="Obrázok" name="Obrazok">
                                 </div>
                                 <div class="form-group ">
-                                    <label class="text-uppercase font-weight-bold" for="univerzities_id">univerzities_id</label>
-                                    <input type="number" class="form-control rounded-0" id="univerzities_id" placeholder="univerzities_id" name="univerzities_id">
-                                </div>
-                                <div class="form-group ">
-                                    <label class="text-uppercase font-weight-bold" for="krajinies_id">krajinies_id</label>
-                                    <input type="number" class="form-control rounded-0" id="krajinies_id" placeholder="krajinies_id" name="krajinies_id">
-                                </div>
-                                <div class="form-group ">
-                                    <label class="text-uppercase font-weight-bold" for="users_id">users_id</label>
-                                    <input type="number" class="form-control rounded-0" id="users_id" placeholder="users_id" name="users_id">
-                                </div>
-                                <div class="form-group ">
-                                    <label class="text-uppercase font-weight-bold" for="popis">popis</label>
-                                    <input type="text" class="form-control rounded-0" id="popis" placeholder="popis" name="popis">
-                                </div>
-                                <div class="form-group ">
-                                    <label class="text-uppercase font-weight-bold" for="Obrazok">Obrazok</label>
-                                    <input type="text" class="form-control rounded-0" id="Obrazok" placeholder="Obrazok" name="Obrazok">
-                                </div>
-                                <div class="form-group ">
-                                    <input type="hidden" value="0" class="form-control rounded-0" id="confirmed" placeholder="confirmed" name="confirmed">
-                                    <label class="text-uppercase font-weight-bold" for="confirmed">confirmed</label>
-                                    <input type="checkbox"  value="1" class="form-control rounded-0" id="confirmed" placeholder="confirmed" name="confirmed">
+                                    <input type="hidden" value="0" id="confirmed" placeholder="confirmed" name="confirmed">
+                                    <label class="text-uppercase font-weight-bold" for="confirmed">Potvrdené</label>
+                                    <input type="checkbox"  value="1" id="confirmed" placeholder="confirmed" name="confirmed">
                                 </div>
                                 <div class="form-group ">
                                     <button type="submit" class="btn btn-danger text-uppercase rounded-0 font-weight-bold">
-                                        confirm
+                                        Potvrdiť
                                     </button>
                                 </div>
                             </form>
